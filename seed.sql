4344a8db49b0ff36c7ba8f1cc6caf15fd9335570
Jorgebillini <Jorgebillini@pursuit.org>
2019-03-23 01:34:17 -0400

    seed file changes

--- seed.sql
+++ seed.sql
@@ -1,71 +1,99 @@
 DROP DATABASE IF EXISTS eb_market;
 CREATE DATABASE eb_market;
-\c eb_market;
-CREATE TABLE buyers
-(
-    id serial PRIMARY KEY,
-    email VARCHAR unique NOT NULL,
-    username VARCHAR unique NOT NULL,
+ALTER DATABASE eb_market OWNER TO "Super";
+
+\connect eb_market
+
+
+
+CREATE TABLE public.buyers (
+    id integer NOT NULL,
+    email character varying NOT NULL,
+    username character varying NOT NULL,
     password character varying NOT NULL,
     firebase_id character varying,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE shops (
-    id serial PRIMARY KEY,
-    firebase_id  VARCHAR ,
-    shop_handle VARCHAR unique NOT NULL,
-    description VARCHAR NOT NULL,
-    type VARCHAR NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    email VARCHAR unique NOT NULL,
-    password VARCHAR unique NOT NULL
 
-);
-CREATE TABLE categories (
-    name VARCHAR NOT NULL,
-    id  serial PRIMARY KEY
+
+
+CREATE TABLE public.categories (
+    name character varying NOT NULL,
+    id integer NOT NULL
 );
 
-CREATE TABLE products (
-    name VARCHAR NOT NULL ,
-    image_url_array VARCHAR NOT NULL,
-    shop_id INT REFERENCES shops(id) NOT NULL,
-    amount INT NOT NULL,
-    specs VARCHAR not NULL,
-    id  serial PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 
-);
-CREATE TABLE orders(
-    id  serial PRIMARY KEY,
-    buyer_info VARCHAR,
-    total_amount int NOT NULL,
-    payment_info VARCHAR NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+--
+-- TOC entry 2982 (class 0 OID 0)
+-- Dependencies: 200
+-- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
 
 
-);
 
-CREATE TABLE order_item (
-   user_id int REFERENCES buyers(id) NOT NULL,
-   order_id int REFERENCES orders(id) NOT NULL,
-   product_id int REFERENCES products(id) NOT NULL,
-   shop_id int REFERENCES shops(id) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+--
+-- TOC entry 206 (class 1259 OID 25205)
+-- Name: order_item; Type: TABLE; Schema: public; Owner: postgres
+--
 
+CREATE TABLE public.order_item (
+    user_id integer NOT NULL,
+    order_id integer NOT NULL,
+    product_id integer NOT NULL,
+    shopid integer NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO buyers(email,username,password) VALUES
-('lol@pursuit.org','Abdul','897');
-INSERT INTO shops(shop_handle,description,type,email,password) VALUES
-('jorge skates','making badass skates since day1', 'DIY', 'jorge@lol.com','123');
-INSERT INTO orders(buyer_info,total_amount,payment_info,buyerid) VALUES
-('{abdul,lol}',420,'paid',1);
+
+
+--
+-- TOC entry 205 (class 1259 OID 25195)
+-- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders (
+    id integer NOT NULL,
+    buyer_info character varying,
+    total_amount integer NOT NULL,
+    payment_info character varying NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    buyerid integer,
+    shop_id integer
+);
 
 
 
-ALTER TABLE products ALTER COLUMN specs TYPE json USING specs::json;
-INSERT INTO order_item(user_id,order_id,product_id,shop_id) VALUES (1, 1,9,3);
+
+
+
+
+
+
+
+
+CREATE TABLE public.products (
+    name character varying NOT NULL,
+    image_url_array character varying NOT NULL,
+    shop_id integer NOT NULL,
+    amount integer NOT NULL,
+    specs json NOT NULL,
+    id integer NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE public.shops (
+    id integer NOT NULL,
+    firebase_id character varying,
+    shopname character varying NOT NULL,
+    description character varying NOT NULL,
+    type character varying NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    email character varying NOT NULL,
+    password character varying NOT NULL
+);
+

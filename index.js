e7a110821ad331664cb46728e5562695d0692035
Jorgebillini <Jorgebillini@pursuit.org>
2019-04-02 21:30:09 -0400

    cors implementation

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const userApp = require('./routes/user')
 const productApp = require('./routes/product');
 const shopApp = require('./routes/shop');
 // MIDDLEWARE HERE
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 
 // ROUTES HERE
